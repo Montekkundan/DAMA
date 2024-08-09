c9feb12b2d883b211fecc452df2cd6d380b63d86
Montek <87750128+Montekkundan@users.noreply.github.com>
2024-08-09 15:06:50 -0700

    updated structure

---
+++ README.md
@@ -0,0 +1,39 @@
+# Setup
+
+## setup venv enviroment 
+
+for mac: 
+```bash
+source venv/bin/activate
+```
+
+for windows: 
+```bash
+./venv/Scripts/activate
+```
+
+## install requirements
+
+```bash
+pip install -r requirements.txt
+```
+
+# How to run
+
+```bash
+python main.py
+```
+
+# Testing
+
+in root directory run:
+
+```bash
+pytest
+```
+
+for debugging:
+
+```bash
+pytest -s
+```
---
+++ test/test_outputs.py
@@ -0,0 +1,29 @@
+import os
+from chat.main import get_completion
+import pytest
+import json
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+json_path = os.path.join(current_dir, 'test_cases.json')
+
+
+with open(json_path, 'r') as file:
+    test_cases = json.load(file)
+
+test_cases_param = [(case['text'], case['answer']) for case in test_cases]
+
+@pytest.mark.parametrize("query, expected", test_cases_param)
+def test_model_selection(query, expected):
+    result = get_completion(query).strip()
+    
+    print(f"Query: '{query}' | Expected: '{expected}' | Got: '{result}'", end=' ')
+    
+    if result == expected:
+        print("✅") 
+    else:
+        print("❌")
+    
+    assert result == expected
+
+if __name__ == "__main__":
+    pytest.main()
